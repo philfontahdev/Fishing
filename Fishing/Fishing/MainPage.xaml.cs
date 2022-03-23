051c1bd288a2657386b44d427680f1dfafc5bd89
PHIL FONTAH <PHILFONTAH911@GMAIL.COM>
2022-03-23 11:42:35 -0700

    Add project files.

---
+++ Fishing/Fishing/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace Fishing
+{
+    public partial class MainPage : ContentPage
+    {
+        Page onboardingpage;
+        public MainPage()
+        {
+            InitializeComponent();
+            if(ShouldShowOnboarding() == true)
+            {
+                App.Current.ModalPopping += Current_ModalPopping;
+                onboardingpage = new OnboardingPage();
+                Navigation.PushModalAsync(onboardingpage,false);
+            }
+        }
+
+        private void Current_ModalPopping(object sender, ModalPoppingEventArgs e)
+        {
+            if (e.Modal == onboardingpage)
+            {
+                FadeBox.FadeTo(0, 1000);
+                onboardingpage = null;
+                App.Current.ModalPopping -= Current_ModalPopping;
+            }
+        }
+
+        private bool ShouldShowOnboarding()
+        {
+            return true;
+            /*if(VersionTracking.IsFirstLaunchEver)
+            {
+                return true;
+            }else
+            {
+                return false;
+            }*/
+        }
+    }
+}
---
+++ Fishing/Fishing/OnboardingPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using MagicGradients;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Fishing
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class OnboardingPage : ContentPage
+    {
+        public OnboardingPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await FadeBox.FadeTo(1, 1000);
+         
+           await Navigation.PopModalAsync(false);
+        }
+
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            MyRadialGradient.RadiusX = this.Width * e.NewValue;
+        }
+    }
+}
